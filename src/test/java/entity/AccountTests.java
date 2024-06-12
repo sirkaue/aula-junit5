632b2c247872bac5b379fab1ece6f1b85193e859
sirkaue <kauekdslopes@gmail.com>
2024-06-12 20:43:40 -0300

    AccountTests - depositShouldDoNothingWhenNegativeAmount

--- src/test/java/entity/AccountTests.java
+++ src/test/java/entity/AccountTests.java
@@ -6,11 +6,22 @@ import org.junit.jupiter.api.Test;
 public class AccountTests {
 
     @Test
-    public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+    public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
 
         double amount = 200.0;
         double expectedValue = 196.0;
         Account account = new Account(1L, 0.0);
+
+        account.deposit(amount);
+
+        Assertions.assertEquals(expectedValue, account.getBalance());
+    }
+
+    @Test
+    public void depositShouldDoNothingWhenNegativeAmount() {
+        double expectedValue = 100.0;
+        Account account = new Account(1L, expectedValue);
+        double amount = -200.0;
 
         account.deposit(amount);
 
