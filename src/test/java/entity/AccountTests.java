95ba29e583e71290b75946af41fea8dcd45d169d
sirkaue <kauekdslopes@gmail.com>
2024-06-12 20:32:35 -0300

    first commit

---
+++ src/test/java/entity/AccountTests.java
@@ -0,0 +1,19 @@
+package entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountTests {
+
+    @Test
+    public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+
+        double amount = 200.0;
+        double expectedValue = 196.0;
+        Account account = new Account(1L, 0.0);
+
+        account.deposit(amount);
+
+        Assertions.assertEquals(expectedValue, account.getBalance());
+    }
+}
